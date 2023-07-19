2c8d89631692ada6ae994dc7447b0424540f3404
Marin Grabovac <mgrabovac22@student.foi.hr>
2023-07-19 19:14:37 +0200

    #28 Made file movieDAO, filled it with class and methods, optimized uDAO
    Made the methods that would be required when dealing with movies. And optimised usersDAO for the  JSON.

---
+++ node-server/service/movieDAO.js
@@ -0,0 +1,51 @@
+let DB = require("./database");
+
+class movieDAO{
+    constructor(){
+        this.db = new DB();
+    }
+
+    async getALL(){
+        this.db.openConnection();
+        let sql = "SELECT * FROM movie";
+        let result = await this.db.executeQuery(sql);
+        this.db.closeConnection();
+        return result;
+    }
+
+    async get(ID){
+        this.db.openConnection();
+        let sql = "SELECT * FROM movie where idMovie = ?";
+        let result = await this.db.executeQuery(sql, ID);
+        this.db.openConnection();
+        return result;
+    }
+
+    async getElement(element, ID){
+        let sql = `SELECT ${element} from movie where idMovie = ?`;
+        let result = await this.db.executeQuery(sql, ID);
+        return result;
+    }
+
+    async add(movie){
+        let sql = "INSERT INTO movie VALUES(default, ?, ?, ?, ?, ?)";
+        let result = await this.db.executeQuery(sql, [movie.name, movie.year, movie.grade, movie.genre, movie.user]);
+        return result;
+    };
+
+    async update(ID, element, elementValue){
+        let sql = `UPDATE movie SET ${element} = ? WHERE idMovie = ?`;
+        let result = await this.db.executeQuery(sql, [elementValue, ID]);
+        return result;
+    }
+
+    async delete(ID){
+        let sql = "DELETE FROM movie WHERE idMovie = ?";
+        let result = await this.db.executeQuery(sql, ID);
+        return result;
+    }
+};
+
+    
+
+
--- node-server/service/usersDAO.js
+++ node-server/service/usersDAO.js
@@ -7,64 +7,29 @@
 
         async get(element, name){
             this.db.openConnection();
-                let result = await this.db.executeQuery(`SELECT ${element} FROM users WHERE Name = ?`, name);
-
+            let result = await this.db.executeQuery(`SELECT ${element} FROM users WHERE Name = ?`, name);
             this.db.closeConnection();
             return result;
         }
 
-        async add(name, email, date_of_birth, age, password){
+        async add(user){
             let sql = "INSERT INTO users VALUES(default, ?, ?, ?, ?, ?)";
-            this.db.openConnection();
-            let result = await this.db.executeQuery(sql, [name, email, date_of_birth, age, password]);
-            this.db.closeConnection();
+            let result = await this.db.executeQuery(sql, [user.Name, user.email, user.date_of_birth, user.age, user.password]);
             return result;
             
         }
         
         async update(name, element, elementValue){
             let sql = `UPDATE users SET ${element} = ? WHERE Name = ?`;
-            this.db.openConnection();
             let result = await this.db.executeQuery(sql, [elementValue, name]);
-            this.db.closeConnection();
             return result;
         }
 
         async delete(name){
             let sql = "DELETE FROM users WHERE Name = ?";
-            this.db.openConnection();
             let result = await this.db.executeQuery(sql, name);
-            this.db.closeConnection();
             return result;
         }
         
     };
-
-    // #TESTING#
-    let inst = new userDAO();
-
-    inst.get("email", "Marin").then((result) => {
-    console.log(result);
-    }).catch((error) => {
-    console.error('Error:', error);
-    });
-    
-    inst.add("Marin", "grabovac@gmail.com", "2003-12-06", 20, "34000").then((result) => {
-    console.log(result);
-    }).catch((error) => {
-    console.error('Error:', error);
-    });
-    
-    inst.update("Marin", "email", "grabovacmarin@gmail.com").then((result) => {
-    console.log(result);
-    }).catch((error) => {
-    console.error('Error:', error);
-    });
-    
-    inst.delete("Petar").then((result) => {
-    console.log(result);
-    }).catch((error) => {
-    console.error('Error:', error);
-    });
-
 
