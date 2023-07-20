f737aa5cf91a0fbed750566452fc46cf707d3a17
Marin Grabovac <mgrabovac22@student.foi.hr>
2023-07-20 17:47:32 +0200

    #28 Made file showDAO.js and made class inside
    It is the same as the movieDAO.js just with changed names of the columns and tables

---
+++ node-server/service/showDAO.js
@@ -0,0 +1,49 @@
+let DB = require("./database");
+
+class showDAO{
+    constructor(){
+        this.db = new DB();
+    }
+
+    async getALL(){
+        this.db.openConnection();
+        let sql = "SELECT * FROM shows";
+        let result = await this.db.executeQuery(sql);
+        this.db.closeConnection();
+        return result;
+    }
+
+    async get(ID){
+        this.db.openConnection();
+        let sql = "SELECT * FROM shows where idshows = ?";
+        let result = await this.db.executeQuery(sql, ID);
+        this.db.openConnection();
+        return result;
+    }
+
+    async getElement(element, ID){
+        let sql = `SELECT ${element} from shows where idshows = ?`;
+        let result = await this.db.executeQuery(sql, ID);
+        return result;
+    }
+
+    async add(show){
+        let sql = "INSERT INTO shows VALUES(default, ?, ?, ?, ?, ?)";
+        let result = await this.db.executeQuery(sql, [show.name, show.genre, show.rating, show.episodes, show.users]);
+        return result;
+    };
+
+    async update(ID, element, elementValue){
+        let sql = `UPDATE shows SET ${element} = ? WHERE idshows = ?`;
+        let result = await this.db.executeQuery(sql, [elementValue, ID]);
+        return result;
+    }
+
+    async delete(ID){
+        let sql = "DELETE FROM shows WHERE idshows = ?";
+        let result = await this.db.executeQuery(sql, ID);
+        return result;
+    }
+}
+
+    
