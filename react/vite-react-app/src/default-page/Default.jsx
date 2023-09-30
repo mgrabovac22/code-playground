070aebcd8328f5070e520ad96ceaaa8284294560
Marin Grabovac <grabovacmarin000@gmail.com>
2023-09-30 19:46:59 +0200

    #42 Header for default page is finished

--- react/vite-react-app/src/default-page/Default.jsx
+++ react/vite-react-app/src/default-page/Default.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './default.css';
 import { Link } from 'react-router-dom';
 
+import Header from './header-components/Header';
 
 function Default(){
 
     return(
         <>
+            <Header></Header>
             <p>This is default</p>
             <Link to="/login">Go to login</Link>
             <Link to="/register">Go to register</Link>
