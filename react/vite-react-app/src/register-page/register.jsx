b37f81ab2734622f18ceeaded2d5755271de1891
Marin Grabovac <grabovacmarin000@gmail.com>
2023-10-15 12:43:18 +0200

    #42 Footer and frontend for register is finished

--- react/vite-react-app/src/register-page/register.jsx
+++ react/vite-react-app/src/register-page/register.jsx
@@ -3,12 +3,14 @@ import './register.css';
 
 import HeaderReg from './header-components/Header';
 import MainReg from './main-components/Main';
+import FooterReg from './footer-components/Footer';
 function Register(){
 
     return(
         <>
             <HeaderReg></HeaderReg>
             <MainReg></MainReg>
+            <FooterReg></FooterReg>
         </>
     );
 }
