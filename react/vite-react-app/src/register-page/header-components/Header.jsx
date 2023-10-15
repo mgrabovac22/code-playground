5fed49f6606b71b1c077eb579954990b51293029
Marin Grabovac <grabovacmarin000@gmail.com>
2023-10-15 12:28:49 +0200

    #42 Main for register is finished

--- react/vite-react-app/src/register-page/header-components/Header.jsx
+++ react/vite-react-app/src/register-page/header-components/Header.jsx
@@ -7,7 +7,7 @@ function HeaderReg(){
         <div id="header">
             <img id ="img1"src="https://clipart-library.com/img1/864942.png" height="80" width="60"/>
             <div id="h1-div">
-                <h1>Log into the website</h1>
+                <h1>Register on the website</h1>
             </div>
             <img  id ="img2" src="https://clipart-library.com/img1/864942.png"height="80" width="60"/>
         </div>
--- react/vite-react-app/src/register-page/main-components/Main.jsx
+++ react/vite-react-app/src/register-page/main-components/Main.jsx
@@ -1,39 +1,55 @@
 import React from "react";
 import './main.css';
+import { Link } from "react-router-dom";
 
 
 function MainReg(){
 
     return (
         <div id="MainReg">
-            <form id="forma">
+            <div id="img1Reg">
+                <img src="https://cdn.pixabay.com/photo/2013/07/13/12/18/camera-159582_1280.png" height="130" width="150"/>
+            </div>
+            <div id = "formDiv">
+                <form>
 
-                <div class="inputCont">
-                    <h2>Enter your name</h2>
-                    <div class="inputC">
-                        <img class = "icon"src="https://cdn-icons-png.flaticon.com/512/805/805333.png" width="50" height="60"/>
-                        <input class="input-field" type="text" placeholder="Name" name="nameInt"/>
+                    <div class="inputCont">
+                        <h2 class ="labelReg">Enter your name</h2>
+                        <div class="inputC">
+                            <img class = "icon"src="https://cdn-icons-png.flaticon.com/512/805/805333.png" width="50" height="60"/>
+                            <input class="input-field" type="text" placeholder="Name" name="nameInt"/>
+                        </div>
                     </div>
-                </div>
 
-                <div class="inputCont">
-                    <h2>Enter your email</h2>
-                    <div class="inputC">
-                        <img class = "icon" src="https://cdn-icons-png.flaticon.com/512/805/805333.png"width="50" height="60"/>
-                        <input class="input-field" type="text" placeholder="Email" name="emailInt"/>
+                    <div class="inputCont">
+                        <h2 class ="labelReg">Enter your email</h2>
+                        <div class="inputC">
+                            <img class = "icon" src="https://cdn-icons-png.flaticon.com/512/805/805333.png"width="50" height="60"/>
+                            <input class="input-field" type="text" placeholder="Email" name="emailInt"/>
+                        </div>
                     </div>
-                </div>
 
-                <div class="inputCont">
-                    <h2>Enter your password</h2>
-                    <div class="inputC">
-                        <img class = "icon" src="https://cdn-icons-png.flaticon.com/512/805/805333.png" width="50" height="60"/>
-                        <input class="input-field" type="text" placeholder="Password" name="PassInt"/>
+                    <div class="inputCont">
+                        <h2 class ="labelReg">Enter your password</h2>
+                        <div class="inputC">
+                            <img class = "icon" src="https://cdn-icons-png.flaticon.com/512/805/805333.png" width="50" height="60"/>
+                            <input class="input-field" type="text" placeholder="Password" name="PassInt"/>
+                        </div>
                     </div>
-                </div>
 
+                    <div id="buttonCont">
+                        <button type="button" id="submitReg">Register</button>
+                    </div>
 
-            </form>
+                    <div id="backLinkRegCont">
+                        <Link to="/" id="LinkBackReg">Go back</Link>
+                    </div>
+
+                </form>
+            </div>
+            <div id="img2Reg">
+                <img src="https://static.vecteezy.com/system/resources/previews/024/098/186/original/cinema-film-tape-reel-free-png.png"  height="100" width="130"/>
+            </div>
         </div>
     );
 }
