c831acc14b14c26048e8906e7b2fb32b4b5ec67b
Marin Grabovac <grabovacmarin000@gmail.com>
2024-07-03 17:00:52 +0200

    #42 Implemented updating books

--- react/vite-react-app/src/main-page-books/main-page-books.jsx
+++ react/vite-react-app/src/main-page-books/main-page-books.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './main-page-books.css';
 
-function MainBooks(){
-
+function MainBooks() {
     const [books, setBooks] = useState([]);
     const [selectedBook, setSelectedBook] = useState(null);
     const [showAddBook, setShowAddBook] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editableBook, setEditableBook] = useState(null);
     const [newBook, setNewBook] = useState({
         name: '',
         genre: '',
@@ -17,7 +18,7 @@ function MainBooks(){
     });
 
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         const fetchBooks = async () => {
             try {
@@ -75,7 +76,6 @@ function MainBooks(){
         }
     };
 
-    
     const handleCancelAddBook = () => {
         setShowAddBook(false);
         setNewBook({
@@ -93,24 +93,88 @@ function MainBooks(){
             console.error('No book selected for deletion');
             return;
         }
-    
+
         try {
             const response = await fetch(`http://localhost:3000/books/${selectedBook.idBook}`, {
                 method: 'DELETE'
             });
-    
+
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-    
+
             setBooks((prevBooks) => prevBooks.filter(book => book.idBook !== selectedBook.idBook));
             setSelectedBook(null);
         } catch (error) {
             console.error('Error deleting book:', error);
         }
     };
-    
-    
+
+    const handleEditBookClick = (book) => {
+        setIsEditing(true);
+        setEditableBook(book);
+    };
+
+    const handleUpdateBook = async () => {
+        const updates = [
+            { 
+                element: 'book_name', 
+                value: editableBook.book_name 
+            },
+            { 
+                element: 'book_release_year', 
+                value: editableBook.book_release_year 
+            },
+            { 
+                element: 'genre_book', 
+                value: editableBook.genre_book 
+            },
+            { 
+                element: 'author_name', 
+                value: editableBook.author_name 
+            },
+            { 
+                element: 'users', 
+                value: editableBook.users 
+            },
+            { 
+                element: 'grade', 
+                value: editableBook.grade 
+            },
+        ];
+
+        try {
+            for (let update of updates) {
+                const response = await fetch(`http://localhost:3000/books/${editableBook.idBook}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        element: update.element,
+                        elementValue: update.value
+                    })
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+            }
+
+            setBooks((prevBooks) => prevBooks.map(book => book.idBook === editableBook.idBook ? editableBook : book));
+            setIsEditing(false);
+            setEditableBook(null);
+            setSelectedBook(editableBook);
+        } catch (error) {
+            console.error('Error updating book:', error);
+        }
+    };
+
+    const handleCancelUpdateBook = () => {
+        setIsEditing(false);
+        setEditableBook(null);
+    };
+
     const routeChangeToMovies = () => navigate("/movies");
     const routeChangeToShows = () => navigate("/shows");
 
@@ -155,7 +219,7 @@ function MainBooks(){
                     </div>
 
                     <div id="bookInfoCont">
-                        {!showAddBook && selectedBook && (
+                        {!showAddBook && selectedBook && !isEditing && (
                             <div id="booksInfo">
                                 <font size="8">{selectedBook.book_name}</font>
                                 <h2 className="infoBooks">Release Year: {selectedBook.book_release_year}</h2>
@@ -164,7 +228,7 @@ function MainBooks(){
                                 <h2 className="infoBooks">Grade: {selectedBook.grade}</h2>
                                 <div id="buttons">
                                     <button id="deleteButton" onClick={handleDeleteBook}>Delete</button>
-                                    <button id="updateButton">Update</button>
+                                    <button id="updateButton" onClick={() => handleEditBookClick(selectedBook)}>Update</button>
                                 </div>
                             </div>
                         )}
@@ -244,6 +308,80 @@ function MainBooks(){
                             </div>
                         )}
 
+                        {isEditing && editableBook && (
+                            <div id="updateBookForm">
+                                <div className="kontInput">
+                                    <div className="inputAdding">
+                                        <label htmlFor="name">Name</label>
+                                        <input
+                                            type="text"
+                                            value={editableBook.book_name}
+                                            onChange={(e) => setEditableBook({ ...editableBook, book_name: e.target.value })}
+                                            placeholder="Book Name"
+                                            name="name"
+                                        />
+                                    </div>
+                                    <div className="inputAdding">   
+                                        <label htmlFor="year">Year</label>
+                                        <input
+                                            type="text"
+                                            value={editableBook.book_release_year}
+                                            onChange={(e) => setEditableBook({ ...editableBook, book_release_year: e.target.value })}
+                                            placeholder="Release Year"
+                                            name="year"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="kontInput">
+                                    <div className="inputAdding">
+                                        <label htmlFor="genre">Genre</label>
+                                        <input
+                                            type="text"
+                                            value={editableBook.genre_book}
+                                            onChange={(e) => setEditableBook({ ...editableBook, genre_book: e.target.value })}
+                                            placeholder="Genre"
+                                            name="genre"
+                                        />
+                                    </div>
+                                    <div className="inputAdding">  
+                                        <label htmlFor="author">Author</label>
+                                        <input
+                                            type="text"
+                                            value={editableBook.author_name}
+                                            onChange={(e) => setEditableBook({ ...editableBook, author_name: e.target.value })}
+                                            placeholder="Author"
+                                            name="author"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="kontInput">
+                                    <div className="inputAdding">
+                                        <label htmlFor="users">Users</label>
+                                        <input
+                                            type="text"
+                                            value={editableBook.users}
+                                            onChange={(e) => setEditableBook({ ...editableBook, users: e.target.value })}
+                                            placeholder="Users"
+                                            name="users"
+                                        />
+                                    </div>
+                                    <div className="inputAdding">  
+                                        <label htmlFor="grade">Grade</label>
+                                        <input
+                                            type="text"
+                                            value={editableBook.grade}
+                                            onChange={(e) => setEditableBook({ ...editableBook, grade: e.target.value })}
+                                            placeholder="Grade"
+                                            name="grade"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="kontInput">
+                                    <button id="buttonUpdate" onClick={handleUpdateBook}>Save</button>
+                                    <button id="buttonCancel" onClick={handleCancelUpdateBook}>Cancel</button>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </main>
